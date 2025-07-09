cf1c84fa3f5ceedb9000b7f9c935fece189b791d
Oleg Latov <oleg@tgadigital.com>
2025-07-10 00:42:56 +0300

    Fix ISR return address for REPs

--- cpu8088.pas
+++ cpu8088.pas
@@ -1731,9 +1731,18 @@ var
 begin
   if Assigned(InterruptHook) and InterruptHook(Self, ANumber) then Exit;
 
-  Push(Registers.Flags.GetWord);
-  Push(Registers.CS);
-  Push(Registers.IP);
+  Push(Registers.Flags.GetWord, Registers.SS);
+
+  if not FCurrentInstruction.Repeating then
+  begin
+    Push(Registers.CS, Registers.SS);
+    Push(Registers.IP, Registers.SS);
+  end else
+  begin
+    Push(FCurrentInstruction.CS, Registers.SS);
+    Push(FCurrentInstruction.IP, Registers.SS);
+  end;
+
   Registers.Flags.IF_ := False;
   Registers.Flags.TF := False;
 
