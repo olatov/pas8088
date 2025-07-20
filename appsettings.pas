49a5b42c0a3b37d03ecba278c80e39cee396089c
Oleg Latov <oleg@tgadigital.com>
2025-07-20 22:29:53 +0300

    Update default app settings

--- appsettings.pas
+++ appsettings.pas
@@ -91,8 +91,8 @@ var
 begin
   Config := TIniFile.Create(AFileName);
   try
-    Machine.ClockSpeed := Config.ReadInteger('Machine', 'ClockSpeed', 675000);
-    Machine.CpuSpeed := Config.ReadInteger('Machine', 'CpuSpeed', 275000);
+    Machine.ClockSpeed := Config.ReadInteger('Machine', 'ClockSpeed', 1250000);
+    Machine.CpuSpeed := Config.ReadInteger('Machine', 'CpuSpeed', 250000);
     Machine.Ram := EnsureRange(
       Config.ReadInteger('Machine', 'Ram', 640), 128, 640);
     Machine.BiosRom := 'poisk_1991.rom';
