b2da4b8098e861085d723a228a0831c06d1cdc83
Oleg Latov <oleg@tgadigital.com>
2025-07-25 11:16:53 +0300

    Update shader

--- gfx/shader.fs
+++ gfx/shader.fs
@@ -11,6 +11,11 @@ uniform float lines;          // Number of vertical lines (e.g. 200)
 void main() {
     vec4 color = texture(texture0, fragTexCoord);
 
+    float brightness = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+    if (brightness > 0.30 && brightness < 0.38) {
+        color.rgb = vec3(0.125);  // Darker replacement for CGA gray
+    }    
+
     if (enableGrayscale != 0) {
         float gray = dot(color.rgb, vec3(0.263, 0.678, 0.059));
         color.rgb = vec3(gray);
@@ -18,6 +23,11 @@ void main() {
 
     if (enableScanlines != 0) {
         float y = fragTexCoord.y * lines;
+
+        if (brightness < 0.05) {
+            // For pure black, add a bit of "noise" glow
+            color.rgb = vec3(0.05);
+        } else         
 
         // If even line, darken
         if (int(floor(y)) % 2 == 0) {
